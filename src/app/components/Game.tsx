e00954d14663567a5fa49dc83d132220447a2582
Kevin <kevin.06.huang@gmail.com>
2023-06-25 00:52:43 -0700

    Added canvas

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,3 +1,10 @@
+import { useRef } from 'react';
+
 export default function Game() {
-  return <div>adfssf</div>;
+  const canvasRef = useRef(null);
+  return (
+    <div className="w-full h-full bg-blue-500 mt-[-25px]">
+      <canvas ref={canvasRef} width={1000} height={800} />
+    </div>
+  );
 }
