65dd1d4bdb44c8d782bf5fb17c4c9b2cc179a68f
Kevin <kevin.06.huang@gmail.com>
2023-06-25 09:24:09 -0700

    Open a new branch after; this has working auth, canvas, basic keyboard input

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,10 +1,62 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Game() {
   const canvasRef = useRef(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    // Drawing and game logic here
+    const background = new Image();
+    background.src = "/logo.png";
+    background.onload = function() {
+      context.drawImage(background, 0, 0);   
+    };
+
+    const player = {
+      x: canvas.width / 10,
+      y: canvas.height / 10,
+      speed: 50
+    };
+
+    const playerImage = new Image();
+    playerImage.src = "/pika-pix.gif";
+
+    const handleKeyDown = (event) => {
+      const { key } = event;
+
+      if (key === 'ArrowUp' || key === 'w') {
+        player.y -= player.speed;
+      } else if (key === 'ArrowDown' || key === 's') {
+        player.y += player.speed;
+      } else if (key === 'ArrowLeft' || key === 'a') {
+        player.x -= player.speed;
+      } else if (key === 'ArrowRight' || key === 'd') {
+        player.x += player.speed;
+      }
+    };
+
+    const drawPlayer = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(playerImage, player.x, player.y, 50, 50);
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    
+    const gameLoop = setInterval(() => {
+      drawPlayer();
+    }, 1000 / 600);
+
+    return () => {
+      // Clean up or remove event listeners if needed
+      window.removeEventListener('keydown', handleKeyDown);
+      clearInterval(gameLoop);
+    };
+  }, []);
+ 
   return (
-    <div className="w-full h-full bg-blue-500 mt-[-25px]">
-      <canvas ref={canvasRef} width={1000} height={800} />
+    <div>
+      <canvas className="w-full h-full bg-white mt-[-25px]" ref={canvasRef} width={1000} height={800} />
     </div>
   );
 }
