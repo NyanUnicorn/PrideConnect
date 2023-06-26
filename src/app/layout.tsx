532a518a67eaafc74d158b388a0c689f8845df91
Kevin <kevin.06.huang@gmail.com>
2023-06-26 04:56:40 -0700

    Fix layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
   session: Session;
 }) {
+  return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
