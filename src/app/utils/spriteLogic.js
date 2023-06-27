2ac4f4abdd2353ab22c8d6a6726770d201bdca73
Adam Liang <me@adamliang.dev>
2023-06-26 21:46:23 -0400

    cleaned up code and another checkpoint

--- src/app/utils/spriteLogic.js
+++ src/app/utils/spriteLogic.js
@@ -1,15 +1,13 @@
-// Define the size of a frame
-let frameWidth = 96;
-let frameHeight = 97;
-let beginX = 135;
-let beginY = 96;
+const frameWidth = 96;
+const frameHeight = 97;
+const beginX = 135;
+const beginY = 96;
+const frameOffset = 1017;
 
-let frameOffset = 1017;
+const playerImageIdOffsetX = [0, 110];
+const playerImageIdOffsetY = [0, 0, 134, 134, 268, 268, 396, 396, 530, 530];
 
-let playerImageIdOffsetX = [0, 110];
-let playerImageIdOffsetY = [0, 0, 134, 134, 268, 268, 396, 396, 530, 530];
-
-let playerLeftOffsetX = [
+const playerLeftOffsetX = [
   [0, -3, 0, 10],
   [-5, 6, 0, -6],
   [0, 0, 0, 8],
@@ -21,7 +19,8 @@ let playerLeftOffsetX = [
   [0, 1, 0, 16],
   [6, 16, 0, 6],
 ];
-let playerLeftOffsetY = [
+
+const playerLeftOffsetY = [
   [0, -5, -5, -5],
   [0, -8, 0, -2],
   [0, 0, -4, 0],
@@ -41,36 +40,47 @@ const background = new Image();
 background.src = "/grass.png";
 
 const drawBackground = (context, canvas) => {
-  if(background.complete) { // Ensure the image has been loaded
+  if (background.complete) {
     context.drawImage(background, 0, 0, canvas.width, canvas.height);
   }
-}
+};
 
 const drawPlayer = (context, canvas, playerImageId, dir, currentFrame, x, y, width, height) => {
-  
   let horizontalOffset = 0;
   let horizontalFrameOffset = 0;
-  let verticalOffset = 0;
   let verticalFrameOffset = 0;
-  switch(dir) {
-    case 'left':
+
+  switch (dir) {
+    case "left":
       horizontalOffset = 757;
       break;
-    case 'right':
+    case "right":
       horizontalOffset = 0;
       break;
-    case 'up':
+    case "up":
       horizontalOffset = 549;
       break;
-    case 'down':
+    case "down":
       horizontalOffset = 277;
       break;
   }
-  if(dir === 'left') {
+
+  if (dir === "left") {
     horizontalFrameOffset = playerLeftOffsetX[playerImageId][currentFrame];
-    verticalFrameOffset = playerLeftOffsetY[playerImageId][currentFrame]
+    verticalFrameOffset = playerLeftOffsetY[playerImageId][currentFrame];
   }
-  context.drawImage(playerImage, beginX + frameOffset * currentFrame + horizontalOffset + horizontalFrameOffset + playerImageIdOffsetX[playerImageId % 2], beginY + verticalFrameOffset + playerImageIdOffsetY[playerImageId], frameWidth, frameHeight, x, y, width, height); 
+
+  context.drawImage(
+    playerImage,
+    beginX + frameOffset * currentFrame + horizontalOffset + horizontalFrameOffset + playerImageIdOffsetX[playerImageId % 2],
+    beginY + verticalFrameOffset + playerImageIdOffsetY[playerImageId],
+    frameWidth,
+    frameHeight,
+    x,
+    y,
+    width,
+    height
+  );
 };
 
 export { drawBackground, drawPlayer };
